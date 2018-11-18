7370f261d61a9b1566ab075ab811a1b4245c66cf
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-11-18 03:43:13 +0100

    Fix double data keyword

--- server/app/controllers/api/v1/users_controller.rb
+++ server/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   def all
-    ok(data: User.all.map { |u| {
+    ok(User.all.map { |u| {
       email: u.email,
       id: u.global_id,
       name: u.name,
