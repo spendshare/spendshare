ae12168f51672bb02b08e53069617f5c69431985
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-11-18 14:38:24 +0100

    Add more bill tests

--- server/app/models/bill.rb
+++ server/app/models/bill.rb
@@ -1,7 +1,10 @@
 class Bill < ApplicationRecord
   has_many :users
+  belongs_to :payer, class_name: 'User'
+  belongs_to :added_by, class_name: 'User'
   alias_attribute :participants, :users
-  validates :title, :added_by_id, :amount, presence: true
+
+  validates :title, :payer, :added_by, :amount, presence: true
   validate :global_ids_are_possible
 
   def create
@@ -27,6 +30,7 @@ class Bill < ApplicationRecord
   end
 
   def check_global_id(global_id)
+    return false if global_id == nil
     type, id = Base64.decode64(payer).split(':')
     return false if type != 'User'
     return false if (id =~ /^[0-9]+$/) == nil
