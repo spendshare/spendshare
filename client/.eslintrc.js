d00844489361b781d8a9b90aa1de6b7a6b577598
osdnk <micosa97@gmail.com>
2018-11-21 23:35:44 +0100

    Eslint rules

---
+++ client/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-hooks"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "no-unused-vars": 0,
+        "react/jsx-uses-vars": 2,
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": 0,
+        "react-hooks/rules-of-hooks": "error"
+    }
+};
