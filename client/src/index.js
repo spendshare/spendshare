ece2a48f14d3bc5a98540f8afd6e669a4e24d0a3
osdnk <micosa97@gmail.com>
2018-11-21 21:13:51 +0100

    Add polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import "@babel/polyfill"
 
 import App from './components/App'
 import store from './store/store'
