06053ca42fe5efa41a2c7fe8456597e39bd74172
osdnk <micosa97@gmail.com>
2018-11-27 20:54:29 +0100

    Add rails admin

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   match '*path',
-    controller: 'application',
-    action: 'options',
-    via: :options
+        controller: 'application',
+        action: 'options',
+        via: :options
 
   namespace 'api' do
     namespace 'v1' do
       post 'session', to: 'sessions#get'
       delete 'session', to: 'sessions#delete'
+      get 'bills', to: 'bills#all'
       post 'bill', to: 'bills#create'
       get 'users', to: 'users#all'
     end
