db462a9e0228d742ddcf2dfd64bd24f340d02c07
osdnk <micosa97@gmail.com>
2018-11-27 20:56:03 +0100

    Remove unrelated code

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
     namespace 'v1' do
       post 'session', to: 'sessions#get'
       delete 'session', to: 'sessions#delete'
-      get 'bills', to: 'bills#all'
       post 'bill', to: 'bills#create'
       get 'users', to: 'users#all'
     end
