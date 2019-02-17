9e530b8ba5880b2878298d58b90c47e6523267a5
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2019-02-17 12:48:54 +0100

    Add models

--- server/db/migrate/20190217111902_initialize.rb
+++ server/db/migrate/20190217111902_initialize.rb
@@ -8,31 +8,35 @@ class Initialize < ActiveRecord::Migration[5.2]
       t.boolean 'deleted'
       t.timestamps
     end
+
     create_table 'participations' do |t|
       t.decimal 'amount', precision: 8, scale: 2
       t.references :bill
       t.references :member
       t.timestamps
     end
+
     create_table 'members' do |t|
       t.references :user
       t.references :group
       t.timestamps
     end
+
     create_table 'groups' do |t|
       t.string 'title'
       t.timestamps
     end
+
     create_table 'users' do |t|
       t.string 'name'
       t.string 'email'
       t.timestamps
     end
+
     create_table :tokens do |t|
       t.string :token, index: true
       t.datetime :valid_until
       t.belongs_to :user, index: true
-
       t.timestamps
     end
   end
