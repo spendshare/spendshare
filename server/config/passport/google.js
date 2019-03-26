653479ab4ae3f3a0dc892548884cfb4ca20b8243
osdnk <micosa97@gmail.com>
2019-03-27 00:01:55 +0100

    Try

---
+++ server/config/passport/google.js
@@ -0,0 +1,27 @@
+/**
+ * Module dependencies.
+ */
+
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://www.example.com/auth/google/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function(err, user) {
+        return done(err, user);
+      });
+    }
+  )
+);
