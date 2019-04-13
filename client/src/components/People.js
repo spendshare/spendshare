c78c27e312565385c7f0560d2a443b647350c536
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2019-04-14 01:25:40 +0200

    Clean console.logs

--- client/src/components/People.js
+++ client/src/components/People.js
@@ -4,22 +4,16 @@ import styles from './People.module.scss'
 import User from './User'
 import { getGroupUsers } from '../store/selectors'
 
-const mapStateToProps = (state, props) => {
-  console.log(props, 'pp')
-  return {
-    users: getGroupUsers(state, props.groupId),
-  }
-}
+const mapStateToProps = (state, props) => ({
+  users: getGroupUsers(state, props.groupId),
+})
 
-const People = ({ users }) => {
-  console.log(users)
-  return (
-    <div className={styles.people}>
-      {users.map(u => (
-        <User key={`user-${u._id}`} user={u} />
-      ))}
-    </div>
-  )
-}
+const People = ({ users }) => (
+  <div className={styles.people}>
+    {users.map(u => (
+      <User key={`user-${u._id}`} user={u} />
+    ))}
+  </div>
+)
 
 export default connect(mapStateToProps)(People)
--- client/src/components/ShowGroup.js
+++ client/src/components/ShowGroup.js
@@ -16,13 +16,10 @@ const ShowGroup = ({ match, requestGroupMembers, groups }) => {
     requestGroupMembers(match.params.id)
   }, [])
 
-  console.log('groups', groups)
   const group = groups[match.params.id]
 
   if (!group || group.loading) return <div>loading...</div>
   if (group.error) return <div>error</div>
-
-  console.log('g', group)
 
   return (
     <div className={styles.container}>
