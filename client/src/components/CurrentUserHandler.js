dce1f83e34de33602c21196e8a014118f0084d33
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2019-04-15 00:11:43 +0200

    Add ability to add bills (#20)

--- client/src/components/CurrentUserHandler.js
+++ client/src/components/CurrentUserHandler.js
@@ -1,27 +1,30 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import actions from '../store/actions'
-import Button from './Button'
 import styles from './CurrentUserHandler.module.scss'
 import { getAvatar } from '../utils'
 
 const CurrentUserHandler = ({ currentUser, dispatch }) => {
+  if (!currentUser) return null
   return (
     <div className={styles.navigation}>
+      <div className={styles.avatar}>
+        <img src={getAvatar(currentUser)} />
+      </div>
       <div className={styles.right}>
-        {currentUser}
-        <br/>
-        {currentUser &&
-          <div
-            className={styles['sign-out']}
-            onClick={() => dispatch(actions.requestSignOut())}
-          >
-            Sign out
-          </div>
-        }
+        {currentUser.name}
+        <br />
+        <div
+          className={styles['sign-out']}
+          onClick={() => dispatch(actions.requestSignOut())}
+        >
+          Sign out
+        </div>
       </div>
     </div>
   )
 }
 
-export default connect(({ users: { currentUser } }) => ({ currentUser }))(CurrentUserHandler)
+export default connect(({ users: { currentUser } }) => ({ currentUser }))(
+  CurrentUserHandler
+)
