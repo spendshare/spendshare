8dac2136d399ed6d3a0a65b2bb020fa5d1df3fee
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2019-04-14 20:06:39 +0200

    Fix paid by on bill list

--- client/src/components/Bill.js
+++ client/src/components/Bill.js
@@ -3,12 +3,17 @@ import classNames from 'classnames'
 import styles from './Bill.module.scss'
 import { parseDate } from '../utils'
 import { currency } from '../config'
+import { connect } from 'react-redux'
 
-export default ({ bill }) => {
+const mapStateToProps = state => ({ users: state.users.all })
+
+const Bill = ({ bill, users }) => {
   const [expanded, setExpanded] = useState(false)
   const toggleExpanded = () => {
     setExpanded(!expanded)
   }
+
+  const paidBy = users[bill.paid.userId]
 
   return (
     <div className={styles.bill} onClick={toggleExpanded}>
@@ -18,13 +23,15 @@ export default ({ bill }) => {
           <span className={styles.large}>{bill.title}</span>
         </div>
         <div className={classNames(styles.column, styles['to-right'])}>
-          <span className={styles.minor}>{bill.paid.name} paid</span>
+          {paidBy && <span className={styles.minor}>{paidBy.name} paid</span>}
           <div className={styles.medium}>
             <span>{bill.paid.amount}</span> <span>{currency}</span>
           </div>
         </div>
       </div>
-      {expanded && <div className="expanded">More info will come here...</div>}
+      {expanded && <div className={styles.expanded}>Nothing to show</div>}
     </div>
   )
 }
+
+export default connect(mapStateToProps)(Bill)
