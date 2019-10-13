e712a858f2283d1c42a7c84b7ede49bdfb16eeef
osdnk <micosa97@gmail.com>
2019-10-13 02:34:06 +0200

    change 4 spaces to 2

--- gurobi/src/main/java/Server.java
+++ gurobi/src/main/java/Server.java
@@ -11,219 +11,210 @@ import org.takes.http.Exit;
 import org.takes.http.FtBasic;
 import org.takes.rq.RqPrint;
 import org.takes.rs.RsJson;
+
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
-import javax.json.JsonObjectBuilder;
-import java.io.IOException;
-import java.io.InputStream;
 
+public final class Server {
 
-public final class Server
-{
+  public static void main(final String... args) throws Exception {
+    new FtBasic(
+      new TkFork(new FkRegex("/", new TkIndex())), 8080
+    ).start(Exit.NEVER);
 
-    public static void main(final String... args) throws Exception {
-        new FtBasic(
-                new TkFork(new FkRegex("/", new TkIndex())), 8080
-        ).start(Exit.NEVER);
+  }
 
+  static long[][] compute(double[] input, int[][] dislikes) throws Exception {
+    GRBEnv env = new GRBEnv();
+    GRBModel model = new GRBModel(env);
+
+    int inputLength = input.length;
+    GRBLinExpr[] eqExprs = new GRBLinExpr[inputLength];
+    GRBQuadExpr[] eqExprs2 = new GRBQuadExpr[inputLength];
+    GRBVar[][] result = new GRBVar[inputLength][inputLength];
+    GRBVar[][] isExisting = new GRBVar[inputLength][inputLength];
+    for (int i = 0; i < result.length; i++) {
+      eqExprs[i] = new GRBLinExpr();
+      eqExprs2[i] = new GRBQuadExpr();
+      for (int j = 0; j < result[i].length; j++) {
+        if (i == j) {
+          continue;
+        }
+        GRBVar x = model.addVar(-10000, 10000, 0.0, GRB.INTEGER, "res-" + i + "-" + j);
+        result[i][j] = x;
+        GRBVar isExistingVar = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "is-used-" + i + "-" + j);
+        isExisting[i][j] = isExistingVar;
+        if (result[j][i] != null) {
+          GRBLinExpr eq = new GRBLinExpr();
+          eq.addTerm(1.0, x);
+          eq.addTerm(1.0, result[j][i]);
+          model.addConstr(eq, GRB.EQUAL, 0, "eq-array-" + i + '-' + 'j');
+          GRBLinExpr eqi = new GRBLinExpr();
+          eqi.addTerm(1.0, isExistingVar);
+          eqi.addTerm(-1.0, isExisting[j][i]);
+          model.addConstr(eqi, GRB.EQUAL, 0, "eq-ex-array-" + i + '-' + 'j');
+
+        }
+        eqExprs[i].addTerm(1.0, x);
+        eqExprs2[i].addTerm(1.0, isExistingVar, x);
+      }
+      model.addQConstr(eqExprs2[i], GRB.EQUAL, input[i], "ea-user-" + i);
     }
 
-    static long[][] compute(double[] input, int[][] dislikes) throws Exception
-    {
-        GRBEnv env = new GRBEnv();
-        GRBModel model = new GRBModel(env);
-
-        int inputLength = input.length;
-        GRBLinExpr[] eqExprs = new GRBLinExpr[inputLength];
-        GRBQuadExpr[] eqExprs2 = new GRBQuadExpr[inputLength];
-        GRBVar[][] result = new GRBVar[inputLength][inputLength];
-        GRBVar[][] isExisting = new GRBVar[inputLength][inputLength];
-        for (int i = 0; i < result.length; i++) {
-            eqExprs[i] = new GRBLinExpr();
-            eqExprs2[i] = new GRBQuadExpr();
-            for (int j = 0; j < result[i].length; j++) {
-                if(i == j) {
-                    continue;
-                }
-                GRBVar x = model.addVar(-10000, 10000, 0.0, GRB.INTEGER, "res-" + i + "-" + j);
-                result[i][j] = x;
-                GRBVar isExistingVar = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, "is-used-" + i + "-" + j);
-                isExisting[i][j] = isExistingVar;
-                if (result[j][i] != null) {
-                    GRBLinExpr eq = new GRBLinExpr();
-                    eq.addTerm(1.0, x);
-                    eq.addTerm(1.0, result[j][i]);
-                    model.addConstr(eq, GRB.EQUAL, 0, "eq-array-" + i + '-' +'j');
-                    GRBLinExpr eqi = new GRBLinExpr();
-                    eqi.addTerm(1.0, isExistingVar);
-                    eqi.addTerm(-1.0, isExisting[j][i]);
-                    model.addConstr(eqi, GRB.EQUAL, 0, "eq-ex-array-" + i + '-' +'j');
-
-                }
-                eqExprs[i].addTerm(1.0, x);
-                eqExprs2[i].addTerm(1.0, isExistingVar, x);
-            }
-            model.addQConstr(eqExprs2[i], GRB.EQUAL, input[i], "ea-user-" + i);
-        }
-
-        for (int i = 0; i < dislikes.length; i++) {
-            GRBLinExpr expr = new GRBLinExpr();
-            expr.addTerm(1.0, isExisting[dislikes[i][0]][dislikes[i][1]]);
-            model.addConstr(expr, GRB.EQUAL, 0, "dislike" + dislikes[i][0] + '-' +  dislikes[i][1]);
-        }
-
-        GRBLinExpr minimize = new GRBLinExpr();
-
-        for (int i = 0; i < isExisting.length; i++) {
-            for (int j = 0; j < isExisting[i].length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                minimize.addTerm(1.0, isExisting[i][j]);
-            }
-        }
-        model.setObjective(minimize, GRB.MINIMIZE);
-
-        model.optimize();
-
-
-
-
-        long[][] res = new long[inputLength][inputLength];
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; j++) {
-                if (i == j) {
-                    res[i][j] = 0;
-                    continue;
-                }
-                res[i][j] = Math.round(result[i][j].get(GRB.DoubleAttr.X) * isExisting[i][j].get(GRB.DoubleAttr.X));
-            }
-        }
-
-        model.dispose();
-        env.dispose();
-
-        return res;
+    for (int i = 0; i < dislikes.length; i++) {
+      GRBLinExpr expr = new GRBLinExpr();
+      expr.addTerm(1.0, isExisting[dislikes[i][0]][dislikes[i][1]]);
+      model.addConstr(expr, GRB.EQUAL, 0, "dislike" + dislikes[i][0] + '-' + dislikes[i][1]);
     }
+
+    GRBLinExpr minimize = new GRBLinExpr();
+
+    for (int i = 0; i < isExisting.length; i++) {
+      for (int j = 0; j < isExisting[i].length; j++) {
+        if (i == j) {
+          continue;
+        }
+        minimize.addTerm(1.0, isExisting[i][j]);
+      }
+    }
+    model.setObjective(minimize, GRB.MINIMIZE);
+
+    model.optimize();
+
+
+    long[][] res = new long[inputLength][inputLength];
+    for (int i = 0; i < result.length; i++) {
+      for (int j = 0; j < result[i].length; j++) {
+        if (i == j) {
+          res[i][j] = 0;
+          continue;
+        }
+        res[i][j] = Math.round(result[i][j].get(GRB.DoubleAttr.X) * isExisting[i][j].get(GRB.DoubleAttr.X));
+      }
+    }
+
+    model.dispose();
+    env.dispose();
+
+    return res;
+  }
 }
 
 class Balance {
-    @SerializedName("id")
-    @Expose
-    private String id;
-    @SerializedName("value")
-    @Expose
-    private double value;
+  @SerializedName("id")
+  @Expose
+  private String id;
+  @SerializedName("value")
+  @Expose
+  private double value;
 
-    public double getValue() {
-        return value;
-    }
+  double getValue() {
+    return value;
+  }
 
-    public String getId() {
-        return id;
-    }
+  String getId() {
+    return id;
+  }
 }
 
 class Dislike {
-    @SerializedName("a")
-    @Expose
-    private String a;
-    @SerializedName("b")
-    @Expose
-    private String b;
+  @SerializedName("a")
+  @Expose
+  private String a;
+  @SerializedName("b")
+  @Expose
+  private String b;
 
-    public String getA() {
-        return a;
-    }
+  String getA() {
+    return a;
+  }
 
-    public String getB() {
-        return b;
-    }
+  String getB() {
+    return b;
+  }
 }
 
 class Req {
-    @SerializedName("balances")
-    @Expose
-    private Balance[] balances;
+  @SerializedName("balances")
+  @Expose
+  private Balance[] balances;
 
-    @SerializedName("dislikes")
-    @Expose
-    private Dislike[] dislikes;
+  @SerializedName("dislikes")
+  @Expose
+  private Dislike[] dislikes;
 
-    public Balance[] getBalances() {
-        return balances;
-    }
+  Balance[] getBalances() {
+    return balances;
+  }
 
-    public Dislike[] getDislikes() {
-        return dislikes;
-    }
+  Dislike[] getDislikes() {
+    return dislikes;
+  }
 }
 
 
 class TkIndex implements Take {
-    @Override
-    public Response act(final Request request) {
-        try {
-            final String body = new RqPrint(request).printBody();
-            Gson gson = new Gson();
-            Req req = gson.fromJson(body, Req.class);
-            Balance[] balances = req.getBalances();
-            Dislike[] dislikes = req.getDislikes();
-            double plainBalances[] = new double[balances.length];
-            for (int i = 0; i < balances.length; i++) {
-                plainBalances[i] = balances[i].getValue();
-            }
+  @Override
+  public Response act(final Request request) {
+    try {
+      final String body = new RqPrint(request).printBody();
+      Gson gson = new Gson();
+      Req req = gson.fromJson(body, Req.class);
+      Balance[] balances = req.getBalances();
+      Dislike[] dislikes = req.getDislikes();
+      double plainBalances[] = new double[balances.length];
+      for (int i = 0; i < balances.length; i++) {
+        plainBalances[i] = balances[i].getValue();
+      }
 
 
-            int plainDislikes[][] = new int[dislikes.length][2];
+      int plainDislikes[][] = new int[dislikes.length][2];
 
-            for (int i = 0; i < dislikes.length; i++) {
-                int a = -1;
-                for (int j = 0; j < balances.length; j++) {
-                    if (balances[j].getId().equals(dislikes[i].getA())) {
-                        a = j;
-                        break;
-                    }
-                }
-                int b = -1;
-                for (int j = 0; j < balances.length; j++) {
-                    if (balances[j].getId().equals(dislikes[i].getB())) {
-                        b = j;
-                        break;
-                    }
-                }
-                plainDislikes[i][0] = a;
-                plainDislikes[i][1] = b;
-            }
-
-            long[][] res = Server.compute(plainBalances, plainDislikes);
-
-
-
-            JsonArrayBuilder jsonBuilder = Json.createArrayBuilder();
-
-            for (int i = 0; i < res.length; i++) {
-                for (int j = 0; j < res[i].length; j++) {
-                    if (res[i][j] > 0) {
-                        jsonBuilder.add(
-                                Json.createObjectBuilder()
-                                        .add("who", balances[i].getId())
-                                        .add("whom", balances[j].getId())
-                                        .add("what", res[i][j])
-                                        .build()
-                        );
-                    }
-                }
-            }
-
-
-
-            return new RsJson(jsonBuilder.build());
-        } catch (Exception e) {
-            e.printStackTrace();
+      for (int i = 0; i < dislikes.length; i++) {
+        int a = -1;
+        for (int j = 0; j < balances.length; j++) {
+          if (balances[j].getId().equals(dislikes[i].getA())) {
+            a = j;
+            break;
+          }
         }
-        return null;
+        int b = -1;
+        for (int j = 0; j < balances.length; j++) {
+          if (balances[j].getId().equals(dislikes[i].getB())) {
+            b = j;
+            break;
+          }
+        }
+        plainDislikes[i][0] = a;
+        plainDislikes[i][1] = b;
+      }
+
+      long[][] res = Server.compute(plainBalances, plainDislikes);
+
+
+      JsonArrayBuilder jsonBuilder = Json.createArrayBuilder();
+
+      for (int i = 0; i < res.length; i++) {
+        for (int j = 0; j < res[i].length; j++) {
+          if (res[i][j] > 0) {
+            jsonBuilder.add(
+              Json.createObjectBuilder()
+                .add("who", balances[i].getId())
+                .add("whom", balances[j].getId())
+                .add("what", res[i][j])
+                .build()
+            );
+          }
+        }
+      }
+
+
+      return new RsJson(jsonBuilder.build());
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 }
 
 
--- gurobi/src/main/java/SolverTest.java
+++ gurobi/src/main/java/SolverTest.java
@@ -3,34 +3,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 public class SolverTest {
-    public SolverTest() { }
+  public SolverTest() {
+  }
 
-    private long[][] safeCompute(double input[], int[][] dislikes) {
-        try {
-            return Server.compute(input, dislikes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+  private long[][] safeCompute(double input[], int[][] dislikes) {
+    try {
+      return Server.compute(input, dislikes);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    @Test
-    public void evaluatesSimpleExpression() {
-        double[] input = {10, 40, -30, 16, 45, -65, 56, -70, -2};
-        int[][] dislikes = {};
-        long[][] res = safeCompute(input, dislikes);
-        long[][] expected = {
-            {0, -40, 0, 0, -45, 95, 0, 0, 0,},
-            {40, 0, 0, 0, 0, 0, 0, 0, 0,},
-            {0, 0, 0, 0, 0, -30, 0, 0, 0,},
-            {0, 0, 0, 0, 0, 0, 0, 0, 16,},
-            {45, 0, 0, 0, 0, 0, 0, 0, 0,},
-            {-95, 0, 30, 0, 0, 0, 0, 0, 0,},
-            {0, 0, 0, 0, 0, 0, 0, 0, 56,},
-            {0, 0, 0, 0, 0, 0, 0, 0, -70,},
-            {0, 0, 0, -16, 0, 0, -56, 70, 0,}
-        };
+  @Test
+  public void evaluatesSimpleExpression() {
+    double[] input = {10, 40, -30, 16, 45, -65, 56, -70, -2};
+    int[][] dislikes = {};
+    long[][] res = safeCompute(input, dislikes);
+    long[][] expected = {
+      {0, -40, 0, 0, -45, 95, 0, 0, 0,},
+      {40, 0, 0, 0, 0, 0, 0, 0, 0,},
+      {0, 0, 0, 0, 0, -30, 0, 0, 0,},
+      {0, 0, 0, 0, 0, 0, 0, 0, 16,},
+      {45, 0, 0, 0, 0, 0, 0, 0, 0,},
+      {-95, 0, 30, 0, 0, 0, 0, 0, 0,},
+      {0, 0, 0, 0, 0, 0, 0, 0, 56,},
+      {0, 0, 0, 0, 0, 0, 0, 0, -70,},
+      {0, 0, 0, -16, 0, 0, -56, 70, 0,}
+    };
 
-        assertArrayEquals(res, expected);
-    }
+    assertArrayEquals(res, expected);
+  }
 }
