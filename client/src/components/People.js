fa23ffcb8ca983d0cb5ba0da11e40c2f881f216f
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2019-11-28 21:30:05 +0100

    Make group view look better

--- client/src/components/People.js
+++ client/src/components/People.js
@@ -6,12 +6,12 @@ import { getGroupUsers } from '../store/selectors'
 
 const mapStateToProps = (state, props) => ({
   users: getGroupUsers(state, props.groupId),
-  debts: state.groupsDebts[props.groupId]
+  debts: state.groupsDebts[props.groupId],
 })
 
-const People = ({ users, groupId, debts }) => (
-  <div className={styles.people}>
-    {debts && users.map(u => (
+const People = ({ users, groupId, faded, debts = {} }) => (
+  <div className={`${styles.people}${faded ? ` ${styles.faded}` : ''}`}>
+    {users.map(u => (
       <User key={u._id} groupId={groupId} debts={debts[u._id]} user={u} />
     ))}
   </div>
